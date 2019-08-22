2ee6161b0255f035498f1c7672b35ce063f9a2f5
Serdar Mustafa <serdarmustafa@Serdars-Yumuuv-MBP.local>
2019-08-22 13:40:27 +0300

    Updates

---
+++ precache-manifest.39bc0080a07c2ee4da76b26d884438b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50ca1b51da0dae97c202",
+    "url": "/lights-out-react/static/css/main.f5f08f1e.chunk.css"
+  },
+  {
+    "revision": "50ca1b51da0dae97c202",
+    "url": "/lights-out-react/static/js/main.50ca1b51.chunk.js"
+  },
+  {
+    "revision": "03109274422a2c9f0101",
+    "url": "/lights-out-react/static/js/1.03109274.chunk.js"
+  },
+  {
+    "revision": "48339403b598306cb307",
+    "url": "/lights-out-react/static/js/runtime~main.48339403.js"
+  },
+  {
+    "revision": "3e4756870c1be9db9459660e4fe902a7",
+    "url": "/lights-out-react/index.html"
+  }
+];
